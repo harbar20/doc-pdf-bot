023c08534910de3e091a5a7529df34d2530c70b9
Harshith Iyer <harbar2021@gmail.com>
2023-09-12 15:30:42 -0400

    able to attach files, and send files

--- src/commands/convert.ts
+++ src/commands/convert.ts
@@ -1,12 +1,22 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandAttachmentOption, SlashCommandBuilder } from "discord.js";
 import { Command } from "../types";
 
 const convert: Command = {
     data: new SlashCommandBuilder()
+        .addAttachmentOption(
+            new SlashCommandAttachmentOption()
+                .setRequired(true)
+                .setName("file")
+                .setDescription("File to convert to PDF")
+        )
         .setName("convert")
         .setDescription("Convert file from one type to another"),
     async execute(interaction, options, client) {
-        await interaction.reply("Test test 123");
+        const attachment = options[0];
+        console.log(attachment);
+        await interaction.reply({
+            files: ["/Users/harbar20/Github/doc-pdf-bot/test.pdf"],
+        });
     },
 };
 
